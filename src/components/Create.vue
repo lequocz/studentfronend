7af284d3f830bae46ebac012dc71ccee600caf7c
lequocz <107678056+lequocz@users.noreply.github.com>
2022-12-07 21:12:41 +0700

    update

---
+++ src/components/Create.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="create">
+  <h1>Cretae</h1>
+    <label for="name">name:</label><br>
+    <input type="text" v-model="form.name"><br>
+    <label for="age">age:</label><br>
+    <input type="text" v-model="form.age"><br>
+    <label for="age">scoreId:</label><br>
+    <input type="text" v-model="form.scoreId"><br>
+    <label for="age">classId:</label><br>
+    <input type="text" v-model="form.classId"><br>
+    <input type="button" value="Submit" @click="save">
+    <form>
+    </form>
+    {{this.form}}
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Create",
+  props: {
+  },data(){
+    return{
+      form:{}
+    }
+  },
+  methods:{
+    save : async function (){
+      const resp = await axios.post('http://localhost:8081/student/save',this.form)
+      this.students = resp.data
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
